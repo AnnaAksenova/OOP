5d9b378a66db0209ab0f9f2a2ac03b63f9982e05
Anna Aksenova <annaa_aksenova@mail.ru>
2021-06-12 22:11:37 +0300

    v1

---
+++ src/main/java/ru/netology/domain/CommentsInfo.java
@@ -0,0 +1,9 @@
+package ru.netology.domain;
+
+public class CommentsInfo {
+    private int count;
+    private boolean canPost;
+    private int groupsCanPost;
+    private boolean CanOpen;
+    private boolean CanClose;
+}
